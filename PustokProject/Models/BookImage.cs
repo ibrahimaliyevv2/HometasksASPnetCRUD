1ac99341dbf1e51395808542af990ce73e87dc5b
ibrahimaliyevv2 <7cauc3b@code.edu.az>
2022-06-07 10:17:49 +0400

    hometasks added

---
+++ PustokProject/Models/BookImage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace PustokProject.Models
+{
+	public class BookImage
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int BookId { get; set; }
+        public bool PosterStatus { get; set; }
+
+        public Book Book { get; set; }
+    }
+}
+
